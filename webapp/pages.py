27b3b54d9807c00af46dc9b5e484977424366f15
tristenwallace <tri3sten@gmail.com>
2024-02-11 07:00:16 -0500

    feat: Make message form and subsequent query page
    Message query renders go.html

--- webapp/pages.py
+++ webapp/pages.py
@@ -1,10 +1,20 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 bp = Blueprint("pages", __name__)
+
+
 
 @bp.route("/")
 def home():
     return render_template("pages/home.html")
+
+@bp.route("/go")
+def response():
+    # save user input in query
+    query = request.args.get('query', '') 
+
+    return render_template("pages/go.html",
+                           query=query)
 
 @bp.route("/about")
 def about():

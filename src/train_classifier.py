99958ef091325c53a32bae03458eb737015a2299
tristenwallace <tri3sten@gmail.com>
2024-02-14 14:31:41 -0500

    feat: Update classification model

--- src/train_classifier.py
+++ src/train_classifier.py
@@ -6,16 +6,23 @@ import re
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import wordpunct_tokenize
-from nltk.stem.porter import PorterStemmer
+from nltk import pos_tag
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import wordnet as wn
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from iterstrat.ml_stratifiers import MultilabelStratifiedKFold as mlsKFold
 from sklearn.pipeline import Pipeline
 from sklearn.multioutput import MultiOutputClassifier
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import LinearSVC
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
 import dill
 
 
+nltk.download('averaged_perceptron_tagger')
+nltk.download("punkt")
 nltk.download("stopwords")
+nltk.download("wordnet")
 
 ##############################################################################
 
@@ -52,7 +59,7 @@ def tokenize(text):
     text (str): text data as str
     
     OUTPUT
-    tokens (list): tokenized text
+    lemma (list): tokenized text
     '''
 
     # Replace all urls with a urlplaceholder string
@@ -72,10 +79,29 @@ def tokenize(text):
     rm = set(stopwords.words("english"))
     tokens= list(set(tokens) - rm)
 
-    # stem tokens
-    tokens =  [PorterStemmer().stem(w) for w in tokens]
+    # Lemmatization w/ POS
+    lemma = []
+    lmtzr = WordNetLemmatizer()
 
-    return tokens
+    dict_pos_map = {
+    # Look for NN in the POS tag because all nouns begin with NN
+    'NN': wn.NOUN,
+    # Look for VB in the POS tag because all verbs begin with VB
+    'VB': wn.VERB,
+    # Look for JJ in the POS tag because all adjectives begin with JJ
+    'JJ' : wn.ADJ,
+    # Look for RB in the POS tag because all adverbs begin with RB
+    'RB': wn.ADV  
+}
+
+    for token, tag in pos_tag(tokens):
+        if tag[0] in dict_pos_map:
+            token = lmtzr.lemmatize(token, pos=dict_pos_map[tag[0]])
+        else:
+            token = lmtzr.lemmatize(token)
+        lemma.append(token)
+
+    return lemma
 
 ##############################################################################
 
@@ -104,13 +130,58 @@ def build_model():
     ''' Return classification model
     '''
 
+    clf = MultiOutputClassifier(LinearSVC())
+
     pipeline = Pipeline([
             ('vect', CountVectorizer(tokenizer=tokenize)),
             ('tfidf', TfidfTransformer()),
-            ('clf', MultiOutputClassifier(KNeighborsClassifier()))
+            ('clf', clf)
         ])
 
-    return pipeline
+    params = {
+        'clf__estimator__C': [1000.0, 2000.0],
+        'clf__estimator__max_iter': [10000],
+        'clf__estimator__random_state': [42],
+        'clf__estimator__dual': ['auto']
+    }
+
+    # Cross validation using grid search
+    cv = GridSearchCV(
+        pipeline, 
+        params, 
+        cv=8,
+        scoring = 'f1_weighted',
+        verbose=1
+    )
+    
+    return cv
+
+##############################################################################
+
+def evaluate_model(model, X_test, Y_test, categories):
+    '''Evaluates models performance in predicting message categories
+    
+    INPUT
+        model (Classification): stored classification model
+        X_test (array): Independent Variables
+        Y_test (array): Dependent Variables
+        category_names (DataFrame): Stores message category labels
+    
+    OUTPUT
+        rints a classification report
+    '''
+    
+    Y_preds = model.predict(X_test)
+
+    # Display Results
+    print('best_score: {}'.format(model.best_score_))
+    print('best_params: {}'.format(model.best_params_))
+
+    print(
+            "Test set classification report: {}".format(
+                classification_report(Y_test, Y_preds, target_names=categories)
+            )
+        )
 
 ##############################################################################
 
@@ -127,7 +198,7 @@ def main():
     if len(sys.argv) == 2:
         
         print('Loading data...\n    DATABASE: /data/DisasterResponse.db')
-        X, Y, category_names = load_data()
+        X, Y, categories = load_data()
 
         print('Splitting data...\n')
         X_train, X_test, Y_train, Y_test = split_data(X, Y)
@@ -137,6 +208,9 @@ def main():
 
         print('Training model...\n')
         model.fit(X_train, Y_train)
+
+        print("Evaluating model...\n")
+        evaluate_model(model, X_test, Y_test, categories)
 
         print('Saving model...\n    MODEL: ../models/message_classifier')
         save_model(model)
